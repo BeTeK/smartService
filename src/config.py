0127ecbbf47adf0a36485be72b05a5e856f797b2
Pasi Luostarinen <pasi.luostarinen@serv.fi>
2021-02-20 11:33:08 +0200

    added example config

---
+++ src/config.py
@@ -0,0 +1,8 @@
+MySqlServer="my host name or ip"
+MySqlDatabase="database name"
+MySqlUsername="database username"
+MySqlPassword="database password"
+
+debug = True
+
+devices=["/dev/sda", "/dev/sdb"]
